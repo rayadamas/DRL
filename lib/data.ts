dfa932b6c7946e188de079fb7c845d979dff8c84
Diamond Louden <djadamasray@gmail.com>
2025-02-02 23:38:48 -0600

    fixed space in 'site pics'

--- lib/data.ts
+++ lib/data.ts
@@ -2,11 +2,11 @@ import React from "react";
 import { CgWorkAlt } from "react-icons/cg";
 import { FaReact } from "react-icons/fa";
 import { LuGraduationCap } from "react-icons/lu";
-import saudaukarBootcampImg from "/Users/djada/Documents/DRL-portfolio-site/site pics/blockchain-developer-bootcamp-final-project_0.png";
-import hyperlyzDemoImg from "/Users/djada/Documents/DRL-portfolio-site/site pics/hyperlyz demo.png";
-import clausuraImg from "/Users/djada/Documents/DRL-portfolio-site/site pics/clausura.png";
-import solanaGameImg from "/Users/djada/Documents/DRL-portfolio-site/site pics/Solana sdk.png";
-import zorbImg from "/Users/djada/Documents/DRL-portfolio-site/site pics/zorb.png";
+import saudaukarBootcampImg from "/Users/djada/Documents/DRL-portfolio-site/sitepics/blockchain-developer-bootcamp-final-project_0.png";
+import hyperlyzDemoImg from "/Users/djada/Documents/DRL-portfolio-site/sitepics/hyperlyz demo.png";
+import clausuraImg from "/Users/djada/Documents/DRL-portfolio-site/sitepics/clausura.png";
+import solanaGameImg from "/Users/djada/Documents/DRL-portfolio-site/sitepics/Solana sdk.png";
+import zorbImg from "/Users/djada/Documents/DRL-portfolio-site/sitepics/zorb.png";
 
 export const links = [
   {
